ebf283e56420c03cbcb069c54d2ddb14e8405306
PI3RRE003 <vitorcruz.dev@gmail.com>
2025-08-06 19:21:16 -0300

    crud com autenticação

---
+++ database.py
@@ -0,0 +1,3 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+#session <- conexão ativa
---
+++ models/user.py
@@ -0,0 +1,8 @@
+from database import db
+from flask_login import UserMixin #autenticação login
+
+class User(db.Model,UserMixin):#tudo que tem no model e mixin
+    #colunas id(int), username(text), password(text)
+    id = db.Column(db.Integer, primary_key=True) #chave primaria = unica
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
